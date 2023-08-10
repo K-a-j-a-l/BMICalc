f3900e174bf611d8d4beb81226b9c208d8e45aac
Kajal <Kajal.mittal08@gmail.com>
2023-08-10 08:44:01 +0530

    Initial commit

---
+++ src/Components/Form.js
@@ -0,0 +1,44 @@
+export default function Form() {
+    return ( <
+        >
+        <
+        div className = "w-full max-w-xs mx-80 my-32" >
+        <
+        form className = "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" >
+        <
+        div className = "mb-4" >
+        <
+        label className = "block text-gray-700 text-sm font-bold mb-2"
+        forName = "Weight" >
+        Weight( in lbs) <
+        /label> <
+        input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id = "Weight"
+        type = "text" / >
+        <
+        /div> <
+        div className = "mb-6" >
+        <
+        label className = "block text-gray-700 text-sm font-bold mb-2"
+        forName = "Height" >
+        Height <
+        /label> <
+        input className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id = "Height"
+        type = "text" / >
+        <
+        /div> <
+        div className = "flex items-center justify-between" >
+        <
+        button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type = "button" >
+        Calculate BMI <
+        /button> <
+        /div> <
+        /form>
+
+        <
+        /div> <
+        />
+    );
+}
